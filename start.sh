da55f0f8dfd7a6ea6f926bccd822d119acdcc9e2
Geoffroy <gefthefrench@gmail.com>
2014-03-06 05:05:26 -0600

    Attempt at caching

--- start.sh
+++ start.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# Starts up Nginx and PHP within the container.
 
-DATADIR="/webapp"
-
-# Don't continue if we catch an error.
-set -e
-
-# Ensure server owns the DATADIR
-chown -R www-data $DATADIR
-
-cd $DATADIR
-
-bundle install
 rake db:create
 rake db:migrate
 rails s -d -p 80
